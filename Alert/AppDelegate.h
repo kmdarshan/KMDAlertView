f38c521904855dd0dc24b3f93bf6969eb627128a
Darshan Katrumane <dkatrumane@shutterfly.com>
2015-02-22 11:55:27 -0800

    Initial Commit

---
+++ Alert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alert
+//
+//  Created by Darshan Katrumane on 2/22/15.
+//  Copyright (c) 2015 Darshan Katrumane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alert
+//
+//  Created by Darshan Katrumane on 2/22/15.
+//  Copyright (c) 2015 Darshan Katrumane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
